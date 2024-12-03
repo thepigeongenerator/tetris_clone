5547cee3142a091df90cccb78fb7732691e5c730
unset <quinnthepigeon@gmail.com>
2024-12-03 12:05:35 +0100

    make a start on the logic

--- src/game/game.c
+++ src/game/game.c
@@ -3,9 +3,20 @@
 #include "../main.h"
 
 
+static void add_shape(const Shape shape, Uint16* row) {
+    for (int i = 0; i < 4; i++) {
+        Uint8 rowData = (shape >> (4 * i)) & 0x0F;
+
+        if (rowData != 0)
+            row[4 - i] |= rowData;
+    }
+}
+
 // called every time the game's state is updated
-void game_update(GameData game_data, const Uint8* keys) {
+void game_update(GameData* game_data, const Uint8* keys) {
     if (keys[SDL_SCANCODE_ESCAPE]) {
         stop();
     }
+
+    add_shape(TETROMINO_I, &game_data->row[4]);
 }
