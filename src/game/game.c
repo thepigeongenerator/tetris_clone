034f1a577df777fc507318b998ad5c57c6ba9ff1
unset <quinnthepigeon@gmail.com>
2025-01-20 00:27:01 +0100

    implement setting coloured shapes

--- src/game/game.c
+++ src/game/game.c
@@ -1,15 +1,27 @@
 #include "game.h"
 
+#include <stdbool.h>
+
 #include "../main.h"
+#include "../window/renderer.h"
+#include "shapes.h"
 
 
-static void add_shape(const Shape shape, Uint16* row) {
-    for (int i = 0; i < 4; i++) {
-        Uint8 rowData = (shape >> (4 * i)) & 0x0F;
+static void _add_shape(Row* row, const Shape shape, const uint8_t colour, const uint8_t pos_x) {
+    for (uint8_t y = 0; y < SHAPE_HEIGHT; y++) {
+        ShapeRow shape_row = shape_get_row(shape, y);
 
-        if (rowData != 0)
-            row[4 - i] |= rowData;
+        if (shape_row == 0)
+            continue;
+
+        for (uint8_t x = 0; x < SHAPE_WIDTH; x++)
+            if (is_set(shape_row, x))
+                row[y].columns[x + pos_x].block = colour;
     }
+}
+
+static inline void add_shape(Row* row, const Shape shape, const uint8_t colour, const uint8_t pos_x, const uint8_t pos_y) {
+    _add_shape(&row[pos_y], shape, colour, pos_x);
 }
 
 // called every time the game's state is updated
@@ -18,5 +30,14 @@ void game_update(GameData* game_data, const Uint8* keys) {
         stop();
     }
 
-    add_shape(TETROMINO_I, &game_data->row[4]);
+    add_shape(game_data->row, TETROMINO_I, CYAN, 1, 0);
+    add_shape(game_data->row, TETROMINO_O, YELLOW, 5, 4);
+
+    add_shape(game_data->row, TETROMINO_Z, GREEN, 1, 8);
+    add_shape(game_data->row, TETROMINO_S, RED, 5, 8);
+
+    add_shape(game_data->row, TETROMINO_J, BLUE, 1, 12);
+    add_shape(game_data->row, TETROMINO_L, YELLOW | RED, 5, 12);
+
+    add_shape(game_data->row, TETROMINO_T, MAGENTA, 5, 16);
 }
--- src/window/renderer.c
+++ src/window/renderer.c
@@ -26,16 +26,6 @@ int renderer_init(SDL_Window** window, SDL_Renderer** renderer) {
     return 0;
 }
 
-static inline int set_block(SDL_Renderer* renderer, const Row row, const uint8_t block, const uint8_t filter, const uint8_t x, const uint8_t y) {
-    uint8_t colour = block & filter;
-    return SDL_SetRenderDrawColor(renderer,
-                                  0xFF * (0 != (colour & (RED | RED << 3))),
-                                  0xFF * (0 != (colour & (GREEN | GREEN << 3))),
-                                  0xFF * (0 != (colour & (BLUE | BLUE << 3))),
-                                  0xFF) |
-           SDL_RenderFillRect(renderer, &(SDL_Rect){x * BLOCK_WIDTH + 1, y * BLOCK_HEIGHT + 1, BLOCK_WIDTH - 1, BLOCK_HEIGHT - 1});
-}
-
 void renderer_update(const RenderData* render_data) {
     SDL_Renderer* renderer = render_data->renderer;
     GameData* data = render_data->game_data;
@@ -43,20 +33,21 @@ void renderer_update(const RenderData* render_data) {
     int success = 0; // if an error occurs, this value is <0
 
     // clear render
-    success |= SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0x00, 0x50);
+    success |= SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0x00, 0xFF);
     success |= SDL_RenderClear(renderer);
 
     for (uint8_t y = 0; y < ROWS; y++) {
         Row row = data->row[y];
 
-        for (uint8_t x = 0; x < COLUMNS; x += 2) {
-            uint8_t block = row >> (x * 3); // get the two blocks stored at this column
-
-            if ((block & 0x0F) != 0)
-                success |= set_block(renderer, row, block, 0x0F, x + 1, y);
-
-            if ((block & 0xF0) != 0)
-                success |= set_block(renderer, row, block, 0xF0, x, y);
+        for (uint8_t x = 0; x < COLUMNS; x++) {
+            if (row.columns[x].block != 0) {
+                success |= SDL_SetRenderDrawColor(renderer,
+                                                  0xFF * !!(row.columns[x].block & RED),
+                                                  0xFF * !!(row.columns[x].block & GREEN),
+                                                  0xFF * !!(row.columns[x].block & BLUE),
+                                                  0xFF);
+                success |= SDL_RenderFillRect(renderer, &(SDL_Rect){x * BLOCK_WIDTH + 1, y * BLOCK_HEIGHT + 1, BLOCK_WIDTH - 1, BLOCK_HEIGHT - 1});
+            }
         }
     }
 
