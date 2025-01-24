ff03fa0e80ba604c9de05ffde528851b4d128104
Quinn <quinnthepigeon@proton.me>
2025-01-24 16:30:32 +0100

    some formatting fixes

--- src/window/colour.h
+++ src/window/colour.h
@@ -1,8 +1,7 @@
 #pragma once
-
-// fits colours with each channel having a width of 2 bits
 #include <stdint.h>
 
+// fits colours with each channel having a width of 2 bits
 typedef union {
     uint8_t packed;
     struct {
@@ -13,10 +12,10 @@ typedef union {
     };
 } Colour;
 
-#define COLOUR_RED      ((Colour){0b11000011})
-#define COLOUR_YELLOW   ((Colour){0b11110011})
-#define COLOUR_ORANGE   ((Colour){0b11100011})
-#define COLOUR_GREEN    ((Colour){0b00110011})
-#define COLOUR_CYAN     ((Colour){0b00111111})
-#define COLOUR_BLUE     ((Colour){0b00001111})
-#define COLOUR_MAGENTA  ((Colour){0b11001111})
+#define COLOUR_RED ((Colour){0b11000011})
+#define COLOUR_YELLOW ((Colour){0b11110011})
+#define COLOUR_ORANGE ((Colour){0b11100011})
+#define COLOUR_GREEN ((Colour){0b00110011})
+#define COLOUR_CYAN ((Colour){0b00111111})
+#define COLOUR_BLUE ((Colour){0b00001111})
+#define COLOUR_MAGENTA ((Colour){0b11001111})
