6aa4840777f9b7e8f343c6250cc6203572d0ff4a
Quinn <quinnthepigeon@proton.me>
2025-03-25 11:33:40 +0100

    improve comments for audio milliseconds computation

--- src/window/audio.c
+++ src/window/audio.c
@@ -185,8 +185,9 @@ audiodata audio_wav_load(audiodevice const* dev, char const* fpath) {
         return (audiodata){0};
     }
 
-    // calculate the time in miliseconds of the audio fragment
     audio.ms = 1000 * (((audio.len) / (SDL_AUDIO_BITSIZE(dev->fmt) / 8)) / spec.channels / dev->freq);
+    // calculate the time in milliseconds of the audio fragment
+    // by dividing the audio bytelength by the format's bitsize, by the audio device's channels and the audio device's frequency
 
     return audio;
 }
