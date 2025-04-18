8fd3de8f7f18ba37f09b6e7e8965c98e07c6513d
Quinn <quinnthepigeon@gmail.com>
2025-04-18 21:27:39 +0200

    write code to improve handling of paths, to be more consistent.
    I don't think I want to place the local data paths there...

---
+++ src/game/paths.h
@@ -0,0 +1,13 @@
+#pragma once
+
+extern char const* restrict path_dat;
+extern char const* restrict path_font;
+extern char const* restrict path_music;
+extern char const* restrict path_place_sfx;
+
+/* initializes the paths, paths are evaluated to NULL upon failure.
+   returns 0 upon success, >0 upon failure <0 upon non-critical failure */
+int paths_init(void);
+
+/* frees allocated data */
+void paths_free(void);
