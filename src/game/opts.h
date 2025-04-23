fb47fd162bd55f50468b0826f60e63028a3330b1
Quinn <quinnthepigeon@gmail.com>
2025-04-23 16:49:30 +0200

    add some options and loading logic

--- src/game/opts.h
+++ src/game/opts.h
@@ -2,4 +2,20 @@
 
 #include <stdint.h>
 
+#include "../window/colour/colour8.h"
+
+struct opts {
+    float sensitivity;      // sensitivity for moving the tetromino around
+    float sensitivity_roll; // sensitivity for rotating the tetromino
+    colour8 colour_O;       // colour for the O shape
+    colour8 colour_I;       // colour for the I shape
+    colour8 colour_S;       // colour for the S shape
+    colour8 colour_Z;       // colour for the Z shape
+    colour8 colour_T;       // colour for the T shape
+    colour8 colour_L;       // colour for the L shape
+    colour8 colour_J;       // colour for the J shape
+};
+
+extern struct opts opts;
+
 int load_opts(void);
